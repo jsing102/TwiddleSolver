2c790ace28dbfd2cfe1a7633fa92b8c258816adf
jsing102 <97690832+jsing102@users.noreply.github.com>
2022-10-02 17:04:47 -0700

    added rotating, did not test

--- BoardSolver.java
+++ BoardSolver.java
@@ -1,3 +1,4 @@
+
 public class BoardSolver {
 
 	private Board board;
@@ -20,11 +21,53 @@ public class BoardSolver {
 	// return something
 	void solve() {
 		System.out.println("Solving...");
-
 	}
 
 	// return something
 	void solveEfficiently() {
 		System.out.println("Solving efficiently...");
+	}
+
+	private void rotateCounterClockwise(int x, int y) {
+		boolean sideOfBoard = checkifCoordinatesonSide(x, y);
+		if (sideOfBoard) {
+			if (y != 0) {
+				y -= 1;
+			}
+			x -= 1;
+		}
+
+		int temp = boardArray[x][y];
+		boardArray[x][y] = boardArray[x][y + 1];
+		boardArray[x][y + 1] = boardArray[x + 1][y + 1];
+		boardArray[x + 1][y + 1] = boardArray[x + 1][y];
+		boardArray[x + 1][y] = temp;
+	}
+
+	private boolean checkifCoordinatesonSide(int x, int y) {
+		boolean sideOfBoard = false;
+		if (x == board.getLength() - 1) {
+			for (int i = 0; i < board.getHeight(); i++) {
+				if (y == i) {
+					sideOfBoard = true;
+				}
+			}
+		}
+		return sideOfBoard;
+	}
+
+	private void rotateClockwise(int x, int y) {
+		boolean sideOfBoard = checkifCoordinatesonSide(x, y);
+		if (sideOfBoard) {
+			if (y != 0) {
+				y -= 1;
+			}
+			x -= 1;
+		}
+		int temp = boardArray[x][y];
+		boardArray[x][y] = boardArray[x - 1][y];
+		boardArray[x - 1][y] = boardArray[x + 1][y - 1];
+		boardArray[x - 1][y - 1] = boardArray[x][y - 1];
+		boardArray[x][y - 1] = temp;
 	}
 }
